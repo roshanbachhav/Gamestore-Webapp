8cee63c7bb4daff0a5c265cb3f4d003002de9e71
roshan hari bachhav <108453526+roshanbachhav@users.noreply.github.com>
2024-07-20 21:42:18 +0530

    Update AdminPortalApplication.java

--- adminPortal/src/main/java/com/adminportal/AdminPortalApplication.java
+++ adminPortal/src/main/java/com/adminportal/AdminPortalApplication.java
@@ -23,20 +23,5 @@ public class AdminPortalApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(AdminPortalApplication.class, args);
 	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		User user1 = new User();
-		user1.setUsername("admin");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("admin"));
-		user1.setEmail("admin@gmail.com");
-		Set<UserRole> userRoles = new HashSet<>();
-		Role role1 = new Role();
-		role1.setRoleId(0);
-		role1.setName("ROLE_ADMIN");
-		userRoles.add(new UserRole(user1, role1));
-
-		userService.createUser(user1 , userRoles);
-	}
 
 }
